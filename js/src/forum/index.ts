d594d65351ee17553fdae1e74644efbf521e8c40
Anthony Ogundipe <diltony@gmail.com>
2024-06-20 18:35:21 +0100

    Updated files

--- js/src/forum/index.ts
+++ js/src/forum/index.ts
@@ -2,4 +2,12 @@ import app from 'flarum/forum/app';
 
 app.initializers.add('dhtml/flarum-language-translator', () => {
   console.log('[dhtml/flarum-language-translator] Hello, forum!');
+
+  if (app.data && app.data.locale) {
+    var currentLocale = app.data.locale;
+    console.log('Current Locale:', currentLocale);
+  } else {
+    console.error('Locale is not defined.');
+  }
+
 });
